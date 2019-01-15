6462855a04ebb3a8ecd4aabdd0638559c6a9fa5c
Julien Debon <julien.debon@decathlon.com>
2019-01-15 13:01:06 +0100

    Clear SLF4J spam when running tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,23 +17,25 @@ repositories {
 
 dependencies {
     // Functional control and immutable data types for Java
-    compile("io.vavr", "vavr", "0.9.2")
+    implementation("io.vavr", "vavr", "0.9.2")
+    // Stop spamming when I run my tests, SLF4J, please, pretty please
+    implementation("org.slf4j", "slf4j-nop", "1.7.25")
     // JUnit 5
     val junitJupiterVersion = "5.3.2"
-    testCompile("org.junit.jupiter", "junit-jupiter-api", junitJupiterVersion)
-    testCompile("org.junit.jupiter", "junit-jupiter-engine", junitJupiterVersion)
-    testCompile("org.junit.jupiter", "junit-jupiter-params", junitJupiterVersion)
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitJupiterVersion)
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", junitJupiterVersion)
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", junitJupiterVersion)
     val junitPlatformVersion = "1.3.2"
-    testCompile("org.junit.platform", "junit-platform-launcher", junitPlatformVersion)
+    testImplementation("org.junit.platform", "junit-platform-launcher", junitPlatformVersion)
     // Fluent test assertion framework
-    testCompile("org.assertj", "assertj-core", "3.11.1")
+    testImplementation("org.assertj", "assertj-core", "3.11.1")
     // Property Based Test framework
     val junitQuickcheck = "0.8.1"
-    testCompile("com.pholser", "junit-quickcheck-core", junitQuickcheck)
-    testCompile("com.pholser", "junit-quickcheck-generators", junitQuickcheck)
-    testCompile("com.github.sir4ur0n", "junit-quickcheck-vavr", "1.0")
+    testImplementation("com.pholser", "junit-quickcheck-core", junitQuickcheck)
+    testImplementation("com.pholser", "junit-quickcheck-generators", junitQuickcheck)
+    testImplementation("com.github.sir4ur0n", "junit-quickcheck-vavr", "1.0")
     // Avoid maintaining the explicit resource file with all Quickcheck generators
-    testCompile("com.google.auto.service", "auto-service", "1.0-rc4")
+    testImplementation("com.google.auto.service", "auto-service", "1.0-rc4")
     testAnnotationProcessor("com.google.auto.service", "auto-service", "1.0-rc4")
 }
 
